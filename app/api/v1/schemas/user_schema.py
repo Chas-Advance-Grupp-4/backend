61368d680c24e1605fd8dd54f11e2a37f2ff39b4
Erik Dahl <erik@iunderlandet.se>
2025-09-12 14:31:13 +0200

    feat: 19 1114 feat to develop and resolve merge conflicts

--- app/api/v1/schemas/user_schema.py
+++ app/api/v1/schemas/user_schema.py
@@ -1,14 +1,16 @@
-from pydantic import BaseModel, constr, field_validator, Field
+from pydantic import BaseModel, field_validator, Field
 from datetime import datetime
-from typing import Literal
+
+
 def not_empty(field_name: str, value: str) -> str:
     if not value.strip():
         raise ValueError(f"{field_name} field empty.")
     return value
 
+
 class UserBase(BaseModel):
     username: str = Field(...)
-    role: Literal["customer", "driver", "admin"] = Field(...)
+
     @field_validator("username")
     def username_not_empty(cls, value):
         return not_empty("Username", value)
@@ -16,17 +18,20 @@ class UserBase(BaseModel):
 
     model_config = {"validate_by_name": True}
 
+
 class UserCreate(UserBase):
     password: str = Field(...)
+
     @field_validator("password")
     def password_not_empty(cls, value):
         return not_empty("Password", value)
+
 
 class UserRead(UserBase):
     id: int
     created_at: datetime
     model_config = {"from_attributes": True}
 
-class UserLogin(BaseModel):
-    username: str
+
+class UserLogin(UserBase):
     password: str
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,16 +1,34 @@
+<<<<<<< HEAD
 from sqlalchemy import Column, String, Integer, DateTime, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime, timezone
+=======
+from sqlalchemy import Column, String, DateTime
 
-Base = declarative_base()
+# from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime, timezone
+from app.db.connection import Base
+import uuid
+
+# Base = declarative_base()
+>>>>>>> origin/feature-19-1114-setup-endpoints-routes-with-auth-sql
+
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = Column(String, unique=True, nullable=False)
+<<<<<<< HEAD
     role = Column(Enum("customer", "driver", "admin", name="user_roles"), nullable=False)
     password_hash = Column(String, nullable=False)
     created_at = Column(DateTime, default=lambda:datetime.now(timezone.utc))
 
 
+=======
+    hashed_password = Column(String, nullable=False)
+    role = Column(
+        String, nullable=False, default="customer"
+    )  # customer | driver | admin
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+>>>>>>> origin/feature-19-1114-setup-endpoints-routes-with-auth-sql
