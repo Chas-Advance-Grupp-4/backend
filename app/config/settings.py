c628aa6541d9b27a4c19ea920a8662eb9dc997e6
alicegmn <alice.eriksson.561@gmail.com>
2025-09-22 12:45:52 +0200

    chore(CORS): update Python version and add CORS middleware for development environment

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,15 +5,16 @@ from pathlib import Path
 
 
 class Settings(BaseSettings):
-
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ENV: str = "development"
+    FRONTEND_URL: str = "http://localhost:5173"
 
     model_config = SettingsConfigDict(
-        env_file=Path(__file__).parent.parent.parent / ".env", extra="ignore"
+        env_file=Path(__file__).parent.parent.parent / ".env",
+        extra="ignore"
     )
-
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
+from app.config.settings import settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.api.v1.routers.router_v1 import router as v1_router
 
+if settings.ENV == "development":
+    allow_origins = [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ]
+else:
+    allow_origins = [settings.FRONTEND_URL]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -9,13 +18,24 @@ async def lifespan(app: FastAPI):
     yield
     print("Application shutdown complete.")
 
-
 app = FastAPI(
-    docs_url="/docs", redoc_url="/redoc", openapi_url="/openapi.json", lifespan=lifespan
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+    lifespan=lifespan
 )
 
-app.include_router(v1_router, prefix="/api/v1")
+# CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+# Routers
+app.include_router(v1_router, prefix="/api/v1")
 
 @app.get("/health")
 async def health_check():
